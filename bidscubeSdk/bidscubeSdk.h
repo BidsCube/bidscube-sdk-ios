4c1325252eec8f1634a61876a1eb4ebf8b17ea9a
Vladyslav <83395350+Villad-dev@users.noreply.github.com>
2025-09-04 15:07:19 +0200

    Initial Commit

---
+++ bidscubeSdk/bidscubeSdk.h
@@ -0,0 +1,18 @@
+//
+//  bidscubeSdk.h
+//  bidscubeSdk
+//
+//  Created by Vladyslav Humennyi on 04/09/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bidscubeSdk.
+FOUNDATION_EXPORT double bidscubeSdkVersionNumber;
+
+//! Project version string for bidscubeSdk.
+FOUNDATION_EXPORT const unsigned char bidscubeSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bidscubeSdk/PublicHeader.h>
+
+
---
+++ bidscubeSdkTests/bidscubeSdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  bidscubeSdkTests.swift
+//  bidscubeSdkTests
+//
+//  Created by Vladyslav Humennyi on 04/09/2025.
+//
+
+import Testing
+@testable import bidscubeSdk
+
+struct bidscubeSdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
